b37a2ef7a76c5e4aba8b42fa82e9ff9eb122e366
herissonsilvahs <herissonsilvahs@gmail.com>
2021-10-12 21:57:36 -0300

    Started

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(PORT)
